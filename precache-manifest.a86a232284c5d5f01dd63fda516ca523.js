61bbb85be279d3b40a0402a63ba9b1dc58f89f31
marcosatan666 <61208408+marcosatan666@users.noreply.github.com>
2020-08-28 13:42:23 +0300

    Added some files......

---
+++ precache-manifest.a86a232284c5d5f01dd63fda516ca523.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b5c825637d81d35541351ddb4381799",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c20ab6cff29c5c2c1b57",
+    "url": "/static/css/2.0d7c217e.chunk.css"
+  },
+  {
+    "revision": "b25b0b42d60627e5db79",
+    "url": "/static/css/main.b4cad330.chunk.css"
+  },
+  {
+    "revision": "c20ab6cff29c5c2c1b57",
+    "url": "/static/js/2.6be1dbcb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6be1dbcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b25b0b42d60627e5db79",
+    "url": "/static/js/main.33d4f88e.chunk.js"
+  },
+  {
+    "revision": "79e76fcaa52a05693c1c",
+    "url": "/static/js/runtime-main.1f6e9381.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
